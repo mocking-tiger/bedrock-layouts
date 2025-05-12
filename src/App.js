f48e444e3f0c0750da9164b0936f3c7a4b2e0994
ParkJunYong <dyden119@gmail.com>
2025-05-12 15:09:04 +0900

    chore: cover, center, app boundary

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import { Stack } from "@bedrock-layout/stack";
+import { Inline } from "@bedrock-layout/inline";
+import { Cover } from "@bedrock-layout/cover";
+import { Center } from "@bedrock-layout/center";
+
 import { Subscribe } from "./Subscribe";
 import { Menu } from "./Menu";
+
 function App() {
   return (
     <Stack gutter="size3">
-      <header>
-        <h1>Coco Le Belle</h1>
-      </header>
-      <Menu />
+      <Cover minHeight="50vh" gutter="size3" top={<Menu />}>
+        <Center maxWidth="40rem">
+          <Stack as="header" gutter="size3">
+            <h1>Coco Le Belle</h1>
+            <p>
+              A collection of handmade, one-of-a-kind jewelry and accessories.
+            </p>
+            <Inline gutter="size3">
+              <button>new Arrivals</button>
+            </Inline>
+          </Stack>
+        </Center>
+      </Cover>
       <Subscribe />
     </Stack>
   );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "@bedrock-layout/css";
+import { AppBoundary } from "@bedrock-layout/appboundary";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppBoundary boundarySize="xlarge">
+      <App />
+    </AppBoundary>
   </React.StrictMode>
 );
